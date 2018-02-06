cb1aafc50e23a6b13ac7d95dc456fc88d3b61079
Park-Ryeong-Min <somuch0321@gmail.com>
2018-02-06 17:23:54 +0900

    commit test

---
+++ configs/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    dbURI : 'mongod://localhost:27017/cluster_exam'
+}
--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 var cluster = require('cluster');
+var http = require('http');
+var mongoose = require('mongoose');
+var express = require('express');
+var dbConfig = require('./configs/db');
+var bodyParser = require('body-parser');
+
 cluster.schedulingPolicy = cluster.SCHED_RR;
+var app = express();
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json());
 
 if(cluster.isMaster){
     const NUM_OF_CORES = require('os').cpus().length;
     console.log('Exam Project - node.js cluster exam');
     console.log('This Computer has ' + NUM_OF_CORES + ' cpu cores.');
     console.log('Master');
+
+    mongoose.connect(dbConfig.dbURI);
+    const db = mongoose.connection;
+    db.on('error', console.error);
+    db.once('open', function () {
+        console.log('connected to mongodb server.');
+    });
+
 
     for(var i = 0; i < NUM_OF_CORES; i++){
         cluster.fork();
@@ -21,4 +39,11 @@ if(cluster.isMaster){
     })
 }else{
 
+    http.createServer(app).listen(10001, function () {
+        console.log('CANDY TREE Http Server listening on port 10001');
+    });
+    console.log('process (pid : ' + process.pid + ') create http server.');
+
 }
+
+// test
